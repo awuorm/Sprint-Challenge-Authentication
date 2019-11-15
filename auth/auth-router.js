80049bc0e4aba6313c77200b0a20ec22628dd14c
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-15 11:52:34 +0300

    created login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const db = require("./authModel");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", (req, res) => {
   const user = req.body;
@@ -19,16 +20,37 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
   db.findBy(username).then(user => {
-    if(user && bcrypt.compareSync(password,user.password)){
-      res.status(200).json({success: `Welcome ${user.username}`});
+    console.log(user);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      console.log("secret",token);
+      res
+        .status(200)
+        .json({ success: `Welcome ${user.username}`, token: token });
+    } else {
+      res.status(403).json({ forbidden: "Please provide a valid password" });
     }
-    else {
-      res.status(403).json({forbidden: "Please provide a valid password"});
-    }
-    
+  }).catch(err => {
+    res.status(500).json({errorMessage: err.message});
+    console.log(err);
   })
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  const result = jwt.sign(payload,"Mildred's secret", options);
+
+  return result;
+}
 
 module.exports = router;
