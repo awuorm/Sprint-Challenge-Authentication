5a361812ada828e3dc45a7ff845f90b2c6f71690
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-15 13:45:03 +0300

    attempting stretch

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Hello from App!
+     
+    </div>
+  );
+}
+
+export default App;
