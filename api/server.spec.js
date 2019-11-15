fdeee8b7fbe71552f70eaf66db508e4cf0f6b602
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-15 12:59:22 +0300

    created login and register tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,13 @@
 const server = require("./server");
-const supertest = require("supertest");
+const request = require("supertest");
 
+
+describe("testing server",() => {
+    describe(" testing default get", () => {
+        test("returns 200 ok", async () => {
+             const response = await request(server).get("/");
+            expect(response.body).toBe("Hello from jokes server!");
+            expect(response.status).toBe(200);
+        })
+    })
+})
