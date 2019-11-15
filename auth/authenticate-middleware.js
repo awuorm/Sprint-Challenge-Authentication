d9c6cf5110171a17ff2cab5990284486d86f513b
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-15 11:56:01 +0300

    created restricted middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,22 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const jwt = require("jsonwebtoken");
+
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, "Mildred's secret", (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: err.message });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+}
+
+module.exports = restricted;
